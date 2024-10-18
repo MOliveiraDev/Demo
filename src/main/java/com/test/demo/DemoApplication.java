1244164899d72a895ce1f008f2fe9f6b59579fd7
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-18 01:36:52 -0300

    Controller

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+
 public class DemoApplication {
 
     public static void main(String[] args) {
