bdf847b125bc14116b1d56902e4b8921364d4c11
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-12-22 16:11:41 -0300

    Update Controller.java

--- src/main/java/com/test/demo/controller/Controller.java
+++ src/main/java/com/test/demo/controller/Controller.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+
 @RestController
 @RequestMapping("/Demo")
 public class Controller {
     @GetMapping
     public String olamundo() {
         return "esse é o meu primeiro código";
+
     }
 }
